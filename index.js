8e64c5d81c4549c9666d093aa818e9def3201718
sohan-me-01 <sohanshak850@gmail.com>
2025-06-03 08:39:16 +0600

    All background file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
@@ -31,6 +31,68 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // job portal database working here
+
+        const jobCollection = client.db('jobPortal').collection('jobs');
+        const jobApplicationCollection = client.db('jobPortal').collection('job_Applications');
+
+        // Get all jobs
+        app.get('/jobs', async (req, res) => {
+            const query = {};
+            const cursor = jobCollection.find(query);
+            const jobs = await cursor.toArray();
+            res.send(jobs);
+        });
+
+        // Get single jobs
+        app.get('/jobDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const job = await jobCollection.findOne(query);
+            res.send(job);
+        });
+
+        // Job Application api
+        app.post('/job-application', async (req, res) => {
+            const application = req.body;
+            console.log('application', application);
+            const result = await jobApplicationCollection.insertOne(application);
+            res.send(result);
+        });
+
+        // Get Some Details form job applications
+        app.get('/job-application', async (req, res) => {
+            const email = req.query.email
+            const query = { userEmail: email };
+            const cursor = jobApplicationCollection.find(query);
+            const applications = await cursor.toArray();
+
+            for (const application of applications) {
+                const jobId = application.jobId;
+                const jobQuery = { _id: new ObjectId(jobId) };
+                const job = await jobCollection.findOne(jobQuery);
+                if (job) {
+                    application.title = job.title;
+                    application.company = job.company;
+                    application.company_logo = job.company_logo;
+                    application.jobType = job.jobType;
+                    application.location = job.location;
+                    application.name = job.hr_name || 'John Doe';
+                }
+            }
+            res.send(applications);
+        });
+
+        // Get Delete a job application
+        app.delete('/job-application/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await jobApplicationCollection.deleteOne(query);
+            res.send(result);
+        });
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
